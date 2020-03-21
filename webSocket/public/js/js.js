614b3642e93c19ab65079ea620771812f8c2d844
DMoloko2 <dmoloko2@gmail.com>
2020-03-21 23:10:33 +0300

    commit fix

--- webSocket/public/js/js.js
+++ webSocket/public/js/js.js
@@ -9,4 +9,84 @@ $(document).ready(function() {
 			$(this).removeClass('nav_left');
 		}
 	);
+
+	$('#pressure_form').submit(function() {
+    var formData = $(this).serialize();
+    $.post('http://172.20.10.10:3000/getvalue',formData,processData);
+    function processData(data) {
+				// console.log(data);
+      }
+			alert('Параметры изменены');
+    return false;
+  });
+
+	$('#humidity_form').submit(function() {
+    var formData = $(this).serialize();
+    $.post('http://172.20.10.10:3000/getvalue',formData,processData);
+    function processData(data) {
+
+      }
+			alert('Параметры изменены');
+    return false;
+  });
+
+	$('#room_temperature_form').submit(function() {
+    var formData = $(this).serialize();
+    $.post('http://172.20.10.10:3000/getvalue',formData,processData);
+    function processData(data) {
+
+      }
+			alert('Параметры изменены');
+    return false;
+  });
+
+	$('#temperature_working_area_form').submit(function() {
+    var formData = $(this).serialize();
+    $.post('http://172.20.10.10:3000/getvalue',formData,processData);
+    function processData(data) {
+
+      }
+			alert('Параметры изменены');
+    return false;
+  });
+
+	$('#ph_form').submit(function() {
+    var formData = $(this).serialize();
+    $.post('http://172.20.10.10:3000/getvalue',formData,processData);
+    function processData(data) {
+
+      }
+			alert('Параметры изменены');
+    return false;
+  });
+
+	$('#weight_form').submit(function() {
+    var formData = $(this).serialize();
+    $.post('http://172.20.10.10:3000/getvalue',formData,processData);
+    function processData(data) {
+
+      }
+			alert('Параметры изменены');
+    return false;
+  });
+
+	$('#flow_rate_form').submit(function() {
+    var formData = $(this).serialize();
+    $.post('http://172.20.10.10:3000/getvalue',formData,processData);
+    function processData(data) {
+
+      }
+			alert('Параметры изменены');
+    return false;
+  });
+
+	$('#CO2_form').submit(function() {
+    var formData = $(this).serialize();
+    $.post('http://172.20.10.10:3000/getvalue',formData,processData);
+    function processData(data) {
+
+      }
+			alert('Параметры изменены');
+    return false;
+  });
 });
